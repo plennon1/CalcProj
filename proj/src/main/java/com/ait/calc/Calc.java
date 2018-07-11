2783eb00f6039d6f68767f3af0c505d0ca6e68af
Paul L <plennon@ait.ie>
2018-07-11 12:31:34 +0100

    New project, core files

---
+++ proj/src/main/java/com/ait/calc/Calc.java
@@ -0,0 +1,16 @@
+package com.ait.calc;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void add(int a, int b)
+	{
+		int total = a + b;
+		//Do something with total
+	}
+
+}
